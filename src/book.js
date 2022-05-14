034bb4a74c0632da4cde60d9c5d90a12bae59720
Anthony Shellman <atshellman@gmail.com>
2022-05-13 20:33:46 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,11 +1,26 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age:age,
+    pronouns: pronouns};
+}
+
+function saveReview(userReview, reviews) {
+  if (!reviews.includes(userReview)) {
+    reviews.push(userReview)
+  }
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
+
+
   // calculatePageCount,
   // writeBook,
   // editBook
