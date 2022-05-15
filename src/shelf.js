f91891524da1002a1cb46a2b46ad9153884832c6
Anthony Shellman <atshellman@gmail.com>
2022-05-15 12:02:18 -0600

    Update shelf and shelf-test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(bookTitle, bookShelf) {
+  if (bookShelf.length < 3) {
+     bookShelf.unshift(bookTitle);
+  }
+  // return bookShelf;
+}
+
+function unshelfBook(bookTitle, bookShelf) {
+  for (var i = 0; i < bookShelf.length; i++)
+    if (bookShelf[i].title.includes(bookTitle)) {
+      bookShelf.splice(i, 1)
+    }
+};
+
+function listTitles(bookShelf) {
+  var titles = [];
+  for (var i = 0; i < bookShelf.length ; i++) {
+    titles.push(bookShelf[i].title)
+  }
+  return titles.join(", ")
+}
+
+function searchShelf(bookShelf, bookTitle) {
+  for (var i = 0; i < bookShelf.length ; i++) {
+    if (bookShelf[i].title === bookTitle) {
+      result = true;
+  } else {
+    result = false;
+  }
+}
+    return result;
+  }
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
