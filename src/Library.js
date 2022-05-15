5cdfee8e07a3a4b01be4d57ff751635318048d78
Anthony Shellman <atshellman@gmail.com>
2022-05-15 17:24:06 -0600

    Update functions

--- src/Library.js
+++ src/Library.js
@@ -21,7 +21,14 @@ function addBook(library, book) {
 }
 
 function checkoutBook(library, book, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++)
+  if (library.shelves[genre][i].title === book) {
+    library.shelves[genre].splice(i, 1)
+    return `You have now checked out ${book} from the ${library.name}`
+  } else {
 
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
 }
 
 module.exports = {
